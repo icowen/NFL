a593e8064a2b701e0c9e95b32617b4ca61ad8170
icowen <ian_cowen@yahoo.com>
2019-11-18 12:29:50 -0600

    40 epochs

--- netTest.py
+++ netTest.py
@@ -20,7 +20,7 @@ class MyTestCase(unittest.TestCase):
         self.net = Net(self.x_train,
                        self.y_train,
                        self.cumsum,
-                       number_of_epochs=1000)
+                       number_of_epochs=40)
 
     def test_train_and_predict(self):
         self.net.train()
